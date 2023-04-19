12e30a0cab82bc76c4c9edd7e24d14543ef480b1
iron-frog <124222884+iron-frog@users.noreply.github.com>
2023-04-19 01:29:20 +0100

    Add all functions to the app
    Able to add books that are displayed as cards. The plus button will
    bring up a form positioned in the middle of the screen and the rest
    of the page are dimmed to make the form stand out. The read status is
    displayed as a button and toggles read and not read.

--- script.js
+++ script.js
@@ -1,42 +1,95 @@
 let myLibrary = [
-  { title: "Foundation", author: "Asimov", pages: "189", read: true },
-  { title: "Second Foundation", author: "Asimov", pages: "227", read: false },
+  new Book("Foundation", "Issac Asimov", "189", true),
+  new Book("Second Foundation", "Issac Asimov", "227", false),
+  new Book("The Naked Sun", "Issac Asimov", "242", true),
 ];
 
 const bookDisplay = document.querySelector(".book-display");
 const form = document.querySelector("form");
+const formButton = document.querySelector("button[type='submit']");
+const bTitle = document.getElementById("input-title");
+const showFormButton = document.querySelector(".show-formBtn");
+const delBook = document.querySelector(".del-book");
+const closeFormBtn = document.querySelector(".close-form");
+const overlay = document.querySelector(".overlay");
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
-    if (read) {
-      return `${title} by ${author}, ${pages} pages, read it`;
-    } else {
-      return `${title} by ${author}, ${pages}, not read it`;
-    }
-  };
 }
 
-function addBookToLibrary() {
-  const title = prompt("Book title");
-  const author = prompt("Author");
-  myLibrary.push({ title, author });
-  return myLibrary;
+Book.prototype.changeRead = function () {
+  this.read = !this.read;
+};
+
+function addBookToLibrary(e) {
+  e.preventDefault();
+  const title = document.getElementById("input-title").value;
+  const author = document.getElementById("input-author").value;
+  const pages = document.getElementById("input-pages").value;
+  const read = document.querySelector("input[name='read']:checked").value;
+  const newBook = new Book(title, author, pages, read);
+  if (title === "" || author === "" || pages === "") {
+    return;
+  }
+  myLibrary.push(newBook);
+  displayBook(myLibrary);
+  formActive();
 }
 
 function displayBook(library) {
-  for (let i = 0; i < library.length; i++) {
-    bookDisplay.innerHTML += `<div class="book-card" data-index="${i}">
-        <h3 class="title">${library[i].title}</h3>
-        <p class="author">${library[i].author}</p>
-        <p class="pages">${library[i].pages}</p>
-        <p>${library[i].read ? "Read" : "Not Read"} </p>
-      </div>`;
+  bookDisplay.innerHTML = myLibrary
+    .map((book, i) => {
+      return `<div class="book-card">
+      
+      <h3 class="title">${book.title}</h3>
+      <div class="book-card-row">
+      <p>Author</p>
+      <p class="author">${book.author}</p>
+      </div>
+      <div class="book-card-row">
+      <p>Page Count</p>
+      <p class="pages">${book.pages}</p>
+      </div>
+      <div class=book-card-row>
+      <p>Status</p>
+      <button class='read-status ${book.read ? "green" : "red"}'
+      data-index="${i}">${book.read ? "Read" : "Not Read"}</button>
+      </div>
+      <button class="del-book" data-index="${i}">-</button>
+    </div>`;
+    })
+    .join("");
+}
+function formActive() {
+  form.classList.toggle("active");
+  overlay.classList.toggle("activebg");
+  form.style.zIndex = "1";
+}
+
+function deleteBook(e) {
+  if (!(e.target.classList == "del-book")) {
+    return;
   }
+  myLibrary.splice(e.target.getAttribute("data-index"), 1);
+  displayBook(myLibrary);
+}
+
+function changeStatus(e) {
+  const buttonTarget = e.target;
+  if (!buttonTarget.matches(".read-status")) {
+    return;
+  }
+  myLibrary[buttonTarget.getAttribute("data-index")].changeRead();
+  displayBook(myLibrary);
 }
 
 displayBook(myLibrary);
-console.log(form.value);
+formButton.addEventListener("click", addBookToLibrary);
+bookDisplay.addEventListener("click", deleteBook);
+bookDisplay.addEventListener("click", changeStatus);
+showFormButton.addEventListener("click", formActive);
+closeFormBtn.addEventListener("click", formActive);
+overlay.addEventListener("click", formActive);
